28aa234b5bf616ab8655d452235ba37585375abb
Mbdx <mbdx64500@gmail.com>
2022-06-21 13:49:15 +0200

    correction

--- dev/mail/contact_me.php
+++ dev/mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'sami.ghazouane@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "sami.ghazouane.io Contact Form:  $name";
+$to = 'marie.boidoux@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Information Luziparc Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: sami.ghazouane.io@ghazouane.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: marie.boidoux@gmail.com "; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
